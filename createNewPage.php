ecf68e8a77685cf62f28a9707251e29c3d64342a
goldenromeo <rommeltrejo@gmail.com>
2016-01-27 11:21:24 -0500

    Wed Jan 27 11:21:24 EST 2016

--- createNewPage.php
+++ createNewPage.php
@@ -1,8 +1,14 @@
 <?php
 
-
-echo "yey";
-
 $site = $_GET["site"];
-    
-echo $site;
+
+checkIfValidURL($site);
+
+//check if string entered is a valid url 
+function checkIfValidURL($siteName){
+    if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+        echo "is url";
+    }else{
+        echo "not an url";
+    }
+}//end check if valid url function
