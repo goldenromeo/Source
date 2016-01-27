c824e0ac9cf1bb310f0c50f96e5a3919116cf421
goldenromeo <rommeltrejo@gmail.com>
2016-01-27 11:27:58 -0500

    Wed Jan 27 11:27:58 EST 2016

--- createNewPage.php
+++ createNewPage.php
@@ -6,9 +6,24 @@ checkIfValidURL($site);
 
 //check if string entered is a valid url 
 function checkIfValidURL($siteName){
-    if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
-        echo "is url";
-    }else{
-        echo "not an url";
-    }
-}//end check if valid url function
+   
+     $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
+    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
+    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
+    $regex .= "(\:[0-9]{2,5})?"; // Port 
+    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
+    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
+    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+    
+     if(preg_match("/^$regex$/", $url)) 
+       { 
+               echo "is url"; 
+       } else {
+         echo " is not an url";
+     }
+    
+    
+    
+    
+    
+}//end check if valid url function. from http://www.php.net/manual/en/function.preg-match.php#93824
