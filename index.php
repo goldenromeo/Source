e9af3ce60809501f2a30f1f927af0a05a2931892
goldenromeo <rommeltrejo@gmail.com>
2016-02-01 13:51:31 -0500

    php script name is now file independent

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!--
-*Rommel Trejo  
-*Source 
+*Rommel Trejo
+*Source
 *Source gives you the public source code of a given webpage
 *github @goldenromeo
 *Feel free to rename the file. the script will still work
@@ -13,16 +13,16 @@
 //variable names
 $site = $_GET["site"];  //name of the website
 $documentName = basename(__FILE__);
-         
+
 //preprocessor
-    //preprocessor was supposed to be the part where the decision to use $site.html or $site/index.html was executed 
+    //preprocessor was supposed to be the part where the decision to use $site.html or $site/index.html was executed
     //through the use of a boolean flag.- This was later dropped and the decision to use a single page was adopted.
     //In the future I would like to expand Source and add this feature but for the moment that feature is out.
     //1/30/16
 
 
 
-//main calls 
+//main calls
 
 //echo the webpage
 echo '<html>
@@ -60,11 +60,11 @@ echo '<html>
 
             </fieldset>
         </form>
-        
-         <!-- end of 
+
+         <!-- end of
         get the adress to be examined form
         -->
-        
+
         </center>
 
     </body>
@@ -85,34 +85,34 @@ validateRequest($site);
 
 //checks if string entered is a valid url: if it has a valid format calls getURL else outputs and error
 function validateRequest($siteName){
-    
+
     //checkif its empty
     if(empty($siteName))
     {return 1;}
-    
+
     //if its not empty check regex
-   
-     $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
-    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
-    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
-    $regex .= "(\:[0-9]{2,5})?"; // Port 
-    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
-    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
-    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
-    
-     if(preg_match("/^$regex$/", $siteName)) 
-       { 
-                
+
+     $regex = "((https?|ftp)\:\/\/)?"; // SCHEME
+    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
+    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
+    $regex .= "(\:[0-9]{2,5})?"; // Port
+    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
+    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
+    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
+
+     if(preg_match("/^$regex$/", $siteName))
+       {
+
                 echo "<code>";
                 echo getURL($siteName);
                 echo "</code";
-         
+
        } else {
          echo "<center> <p> <br> Sorry that is not a URL</p></center>";
      }
-    
-    
-    
+
+
+
 }//end check if valid url function. from http://www.php.net/manual/en/function.preg-match.php#93824
 
 
@@ -120,33 +120,33 @@ function validateRequest($siteName){
 //getURL is in charge of getting the url contents and/or returning an error in case the address entered
 //matches the regex but is not a real web address.
  function getURL($siteName){
-     
-     
-     
+
+
+
      //check if it contains http or https. add http:// if it doesnt contain
-     
+
      if(!preg_match("~^(?:f|ht)tps?://~i",$siteName))
      {
          $siteName = "http://" . $siteName;
-         
+
      }//from http://stackoverflow.com/questions/2762061/how-to-add-http-if-its-not-exists-in-the-url
-     
+
      echo '<br>  <br';
-     
-    
-     
-     
+
+
+
+
     //get page soure code
         $pageLoaded = file_get_contents($siteName);
-     
-        
+
+
             //if exist return source code
             //nl2br ensures line breaks apppear
             //htmlspecialchars ensure html is not interpreted
         return nl2br(htmlspecialchars($pageLoaded)) ;
-        
-     
-     
+
+
+
     }
 
 
