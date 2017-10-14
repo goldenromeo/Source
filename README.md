094ac36347892befca02fe279e4a029aedac399c
Rommel Trejo <rommeltrejo@Rommels-Air.lan>
2017-10-14 17:51:05 -0400

    clean up

---
+++ README.md
@@ -0,0 +1,5 @@
+*  Rommel Trejo
+*  Source
+*  Source gives you the compiled or actual html of a given webpage
+*  github @goldenromeo
+*  Feel free to rename the file. the script will still work
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!--
 *Rommel Trejo
 *Source
-*Source gives you the public source code of a given webpage
+*Source gives you the compiled or actual html of a given webpage
 *github @goldenromeo
 *Feel free to rename the file. the script will still work
-
 -->
 
 
@@ -37,35 +36,36 @@ echo '<html>
     </head>
     <body>
 
-    <center>
+    <div id="Banner">
+        <table>
+	    <tr>
+	    	<td>
+                <iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&repo=Source&type=watch&count=true&size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:170px; height: 30px;" allowTransparency="false"></iframe>
+            </td>
+            <td>
+                <iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&repo=Source&type=fork&count=true&size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:170px; height: 30px;" allowTransparency="false"></iframe>
+            </td>
+            <td>
+                <iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&type=follow&count=true&size=large" height="30" width="240" frameborder="0" scrolling="0" style="width:240px; height: 30px;" allowTransparency="false"></iframe>
+            </td>
+        </tr>
+        </table>
+    </div> 
 
+    <center>
         <h1>Welcome to source</h1>
         <h2>enter your site address below!</h2>
 
-<table><tr><td><iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&repo=Source&type=watch&count=true&size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:170px; height: 30px;" allowTransparency="false"></iframe></td><td><iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&repo=Source&type=fork&count=true&size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:170px; height: 30px;" allowTransparency="false"></iframe></td><td><iframe src="http://ghbtns.com/github-btn.html?user=goldenromeo&type=follow&count=true&size=large" height="30" width="240" frameborder="0" scrolling="0" style="width:240px; height: 30px;" allowTransparency="false"></iframe></td></tr></table>
-
-        <!--
-        get the adress to be examined
-        -->
-
-
+        <!-- get the adress to be examined  -->
 
         <form action="' . $documentName .'" method="get">
-
             <fieldset>
                 <input type="text" name="site" id="site">
                 <br> <br>
-
                 <input type="submit" value="Source">
-
             </fieldset>
         </form>
-
-         <!-- end of
-        get the adress to be examined form
-        -->
-
-        </center>
+    </center>
 
     </body>
 </html>
@@ -74,80 +74,62 @@ echo '<html>
 validateRequest($site);
 
 
-
-
-
-
-        //              ***********************      Functions          ***************************            //
-
-
-
+//              ***********************      Functions          ***************************            //
 
 //checks if string entered is a valid url: if it has a valid format calls getURL else outputs and error
 function validateRequest($siteName){
 
     //checkif its empty
-    if(empty($siteName))
-    {return 1;}
+    if(!empty($siteName))
+    {
+        //if its not empty check regex
 
-    //if its not empty check regex
+        $regex = "((https?|ftp)\:\/\/)?"; // SCHEME
+        $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
+        $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
+        $regex .= "(\:[0-9]{2,5})?"; // Port
+        $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
+        $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
+        $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
 
-     $regex = "((https?|ftp)\:\/\/)?"; // SCHEME
-    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
-    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
-    $regex .= "(\:[0-9]{2,5})?"; // Port
-    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
-    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
-    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
-
-     if(preg_match("/^$regex$/", $siteName))
-       {
+        if(preg_match("/^$regex$/", $siteName))
+        {
 
                 echo "<code>";
                 echo getURL($siteName);
                 echo "</code";
 
-       } else {
-         echo "<center> <p> <br> Sorry that is not a URL</p></center>";
-     }
-
-
-
+        } 
+        else 
+        {
+            echo "<center> <p> <br>Please check the address entered.</p></center>";
+        }
+    }
 }//end check if valid url function. from http://www.php.net/manual/en/function.preg-match.php#93824
 
 
 
 //getURL is in charge of getting the url contents and/or returning an error in case the address entered
 //matches the regex but is not a real web address.
- function getURL($siteName){
+ function getURL($siteName)
+ {
 
+     //add protocol if not specified: use http by default
+    if(!preg_match("~^(?:f|ht)tps?://~i",$siteName))
+    {
+        $siteName = "http://" . $siteName;
+    }//from http://stackoverflow.com/questions/2762061/how-to-add-http-if-its-not-exists-in-the-url
 
+    echo '<br>  <br';
 
-     //check if it contains http or https. add http:// if it doesnt contain
+    //get html
+    $pageLoaded = file_get_contents($siteName);
 
-     if(!preg_match("~^(?:f|ht)tps?://~i",$siteName))
-     {
-         $siteName = "http://" . $siteName;
-
-     }//from http://stackoverflow.com/questions/2762061/how-to-add-http-if-its-not-exists-in-the-url
-
-     echo '<br>  <br';
-
-
-
-
-    //get page soure code
-        $pageLoaded = file_get_contents($siteName);
-
-
-            //if exist return source code
-            //nl2br ensures line breaks apppear
-            //htmlspecialchars ensure html is not interpreted
-        return nl2br(htmlspecialchars($pageLoaded)) ;
-
-
-
-    }
+    //return formatted result
+    //nl2br ensures line breaks apppear
+    //htmlspecialchars ensure html is not interpreted
+    return nl2br(htmlspecialchars($pageLoaded));
+}
 
 
 
